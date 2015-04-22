29b3659e7ddb8812670f3707f78964e524750339
Giau <giaunv@gmail.com>
2015-04-22 11:09:42 +0700

    Project Setup

---
+++ uberjump-spritekit-objc-iphone/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  uberjump-spritekit-objc-iphone
+//
+//  Created by giaunv on 4/22/15.
+//  Copyright (c) 2015 366. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ uberjump-spritekit-objc-iphoneTests/uberjump_spritekit_objc_iphoneTests.m
@@ -0,0 +1,40 @@
+//
+//  uberjump_spritekit_objc_iphoneTests.m
+//  uberjump-spritekit-objc-iphoneTests
+//
+//  Created by giaunv on 4/22/15.
+//  Copyright (c) 2015 366. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface uberjump_spritekit_objc_iphoneTests : XCTestCase
+
+@end
+
+@implementation uberjump_spritekit_objc_iphoneTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
